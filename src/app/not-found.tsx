7068488aebf1c46a2639cb0c1eff9aba7be5c23a
kev1nandreas <kevin.andreascn@gmail.com>
2025-03-10 14:20:19 +0700

    feat: add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div className="flex items-center justify-center h-screen text-5xl font-semibold">
+        <p>404 | Page Not Found</p>
+        </div>
+    );
+}
