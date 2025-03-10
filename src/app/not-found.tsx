a83d0a15ce881c921bcb53c8c3d5803b2195afdf
kev1nandreas <kevin.andreascn@gmail.com>
2025-03-10 17:04:01 +0700

    style: add select-none class to button in not found page for improved user experience

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -13,7 +13,7 @@ export default function NotFound() {
         It seems like you&apos;re lost. Please check your link and try again.
       </p>
       <button
-        className="text-sm font-bold text-blue-500 cursor-pointer border-2 p-2 px-4 rounded-lg hover:bg-blue-500 hover:text-white hover:border-transparent mt-2 transition-all duration-200"
+        className="text-sm font-bold text-blue-500 cursor-pointer border-2 p-2 px-4 rounded-lg select-none hover:bg-blue-500 hover:text-white hover:border-transparent mt-2 transition-all duration-200"
         onClick={() => router.back()}
       >
         <FaArrowLeft className="inline-block w-4 h-4 mr-2" />
