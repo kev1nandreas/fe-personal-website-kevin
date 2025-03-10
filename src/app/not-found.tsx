4a2addd524f99200068f89ee35ee0b4f313e7302
kev1nandreas <kevin.andreascn@gmail.com>
2025-03-10 15:12:08 +0700

    style: fix responsive in not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,13 +7,13 @@ export default function NotFound() {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-5xl font-semibold gap-4">
+    <div className="flex flex-col items-center justify-center h-screen text-3xl md:text-5xl font-semibold gap-4">
       <p>404 | Page Not Found</p>
       <p className="text-sm font-medium">
         It seems like you&apos;re lost. Please check your link and try again.
       </p>
       <button
-        className="text-sm font-bold text-blue-500 cursor-pointer border-2 p-2 px-4 rounded-lg hover:bg-blue-500 hover:text-white hover:border-transparent mt-2"
+        className="text-sm font-bold text-blue-500 cursor-pointer border-2 p-2 px-4 rounded-lg hover:bg-blue-500 hover:text-white hover:border-transparent mt-2 transition-all duration-200"
         onClick={() => router.back()}
       >
         <FaArrowLeft className="inline-block w-4 h-4 mr-2" />
