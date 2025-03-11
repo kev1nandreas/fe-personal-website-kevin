98bd8d5861480f7fd2944eaab97fba101c7d00cf
kev1nandreas <kevin.andreascn@gmail.com>
2025-03-12 01:03:09 +0700

    feat: add about section

---
+++ src/components/Photo.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Image from "next/image";
+import React, { useEffect } from "react";
+import { useState } from "react";
+
+export const Photo = ({ className }: { className?: string }) => {
+  const [active, setActive] = useState(false);
+  const areaRef = React.useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (areaRef.current) {
+      areaRef.current.addEventListener("mouseenter", () => {
+        setActive(true);
+      });
+      areaRef.current.addEventListener("mouseleave", () => {
+        setActive(false);
+      });
+    }
+  }, [areaRef]);
+
+  return (
+    <div className={`${className} relative w-[20rem] h-[20rem]`}>
+      <div
+        className={`absolute overflow-hidden rounded-2xl rotate-10 border-2 border-gray-500 transition-all duration-700 ease-in-out ${
+          active ? "z-10 opacity-100" : "opacity-20"
+        }`}
+      >
+        <Image
+          src={"/BeachKevin.jpg"}
+          alt={"Kevin Beach"}
+          width={200}
+          height={200}
+          className="w-[18rem]"
+        />
+      </div>
+      <div
+        className={`absolute overflow-hidden border-2 -rotate-3 rounded-2xl border-gray-500 transition-all duration-700 ease-in-out ${
+          !active ? "z-10" : "opacity-20"
+        }`}
+      >
+        <Image
+          src={"/BambooKevin.jpg"}
+          alt={"Kevin Bamboo"}
+          width={200}
+          height={200}
+          className="w-[18rem]"
+        />
+      </div>
+      <div
+        ref={areaRef}
+        className="relative -top-5 -left-2 rounded-2xl w-[18rem] h-[18rem] bg-transparent z-20"
+      ></div>
+    </div>
+  );
+};
---
+++ src/containers/About.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/Button/Button";
+import { Photo } from "@/components/Photo";
+import { Typography } from "@/components/Typography";
+import { MdDownload } from "react-icons/md";
+
+export const About = () => {
+  return (
+    <div id="about" className="gap-10 mt-8 flex flex-col items-center justify-center md:flex-row md:gap-15">
+      <div className="flex flex-col gap-7 justify-center items-center md:items-start p-2 max-w-[35rem]">
+        <Typography text={"h1"} className="text-center md:text-left">
+          About <span className="font-dancing-script">Me</span>
+        </Typography>
+
+        <Photo className="md:hidden flex ml-6 mt-12" />
+
+        <Typography
+          text={"body"}
+          className="md:text-justify text-center w-full md:mr-9"
+        >
+          I am a second-year student with a passion for challenges and
+          continuous learning. I am actively improving my skills in informatics,
+          particularly in{" "}
+          <span className="text-yellow-500 font-semibold">data analysis</span>{" "}
+          and{" "}
+          <span className="text-yellow-500 font-semibold">web development</span>{" "}
+          , while also gaining insights into{" "}
+          <span className="text-yellow-500 font-semibold">business</span>.
+          Through my involvement in organizations and committees, I have
+          developed leadership, administration, and time management skills. I am
+          eager to contribute to community activities and further enhance my
+          abilities.
+        </Typography>
+
+        <div className="flex justify-end w-full">
+          <Button className="flex items-center gap-2">
+            <MdDownload className="text-xl"/> Download CV
+          </Button>
+        </div>
+      </div>
+
+      <Photo className="hidden md:block" />
+    </div>
+  );
+};
