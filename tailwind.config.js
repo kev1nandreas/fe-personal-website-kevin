8d11d2d809bd17a4c06c411257a9edb977d8cea7
kev1nandreas <kevin.andreascn@gmail.com>
2025-03-11 21:27:11 +0700

    feat: update dependencies and add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'conic/decreasing': 'conic-gradient(from 180deg, #7C3AED, #84CC16, #7C3AED)',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
