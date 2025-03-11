f91fb2671e748b5cb3f67632de8d28949e62d6e2
kev1nandreas <kevin.andreascn@gmail.com>
2025-03-11 21:36:10 +0700

    feat: update ColorfulButton to use a conic-gradient background

--- src/components/Button/ColorfulButton.tsx
+++ src/components/Button/ColorfulButton.tsx
@@ -19,11 +19,20 @@ export const ColorfulButton = ({
       ${rounded === "full" ? "rounded-full" : "rounded-lg"}`}
     >
       <span
-        className={`absolute inset-0 bg-red-500 min-w-[8rem] 
-        transition-all duration-300 ease-in-out
+        className={`absolute inset-0 min-w-[8rem] transition-all duration-300 ease-in-out
         ${hoverActive && "group-hover:blur-sm group-hover:opacity-80"}
-        
         ${rounded === "full" ? "rounded-full" : "rounded-lg"}`}
+        style={{
+          background: `conic-gradient(
+            from 180deg,
+            rgba(124, 58, 237, 1) 0%,
+            rgba(236, 72, 153, 0.9) 20%,
+            rgba(234, 179, 8, 0.8) 40%,
+            rgba(132, 204, 22, 0.7) 60%,
+            rgba(6, 182, 212, 0.6) 80%,
+            rgba(124, 58, 237, 1) 100%
+          )`,
+        }}
       ></span>
       <span
         className={`relative px-8 py-2 bg-[#171717] text-white font-medium min-w-[8rem] 
