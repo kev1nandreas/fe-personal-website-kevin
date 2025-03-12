4be85b2669cf6a3441b58c89bce3cc8bbf49e874
kev1nandreas <kevin.andreascn@gmail.com>
2025-03-13 00:35:21 +0700

    feat: integrate About, Achievements, Educations, and Experiences sections into the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import { About } from "@/containers/About";
+import { Achievements } from "@/containers/Achievements";
+import { Educations } from "@/containers/Educations";
+import { Experiences } from "@/containers/Experiences";
 import { Hero } from "@/containers/Hero";
 import { Navbar } from "@/containers/Navbar";
 
@@ -6,7 +10,12 @@ export default function Page() {
     <div className="md:p-4 p-3">
       <Navbar />
       <Hero />
-      <div className="h-[30rem]"></div>
+      <About />
+      <Educations />
+      <Achievements />
+      <Experiences />
+      
+      <div className="h-[40rem]"></div>
     </div>
   );
 }
