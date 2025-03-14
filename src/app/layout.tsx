d9bc42489001f12475257426b0ec73ad63581a0e
kev1nandreas <kevin.andreascn@gmail.com>
2025-03-14 18:46:22 +0700

    feat: add custom favicon for the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Kevin Andreas",
   description: "Personal Website | Kevin Andreas",
+  icons: {
+    icon: "/LogoKevin.png"
+  },
 };
 
 export default function RootLayout({
